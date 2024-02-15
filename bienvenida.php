c9cacf226589a2ee046f3a14bf36afd2a4c75a8a
ANDRES VARGAS RIVERA <152044691+andres2342@users.noreply.github.com>
2024-02-14 19:00:51 -0500

    Add files via upload

---
+++ bienvenida.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a La Rivera Cafe</title>
+    <link rel="stylesheet" href="styles_bienvenida.css">
+</head>
+<body>
+           
+
+            <div class="header">
+                <h1>Bienvenido a "La Rivera Cafe" ¡Una experiencia con aroma a cafe!.</h1>
+            </div>
+
+                    
+        <div class="sidebar">
+            <a href="menu_productos.php" class="modulo" id="productos">
+                <h2>Productos</h2>
+                <!-- Contenido del menú -->
+            </a>
+
+                                             
+            <a href="pagina-clientes.html" class="modulo" id="clientes">
+                <h2>Clientes</h2>
+                <!-- Contenido de los clientes -->
+            </a>
+            
+            <a href="pagina-facturas.html" class="modulo" id="facturas">
+                <h2>Facturas</h2>
+                <!-- Contenido de las facturas -->
+            </a>
+            
+            <a href="pagina-galeria.html" class="modulo" id="galeria">
+                <h2>Galería</h2>
+                <!-- Contenido de la galería -->
+            </a>
+            
+            <a href="pagina-quienes-somos.html" class="modulo" id="quienes-somos">
+                <h2>Quiénes Somos</h2>
+                <!-- Contenido sobre quiénes somos -->
+            </a>
+       </div>
+
+    </div>
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; 2024 "Restaurante la rivera cafe". Todos los derechos reservados.</p>
+        </div>
+    </footer>
+
+
+</body>
+</html>
+
+
+	
+
+
+
+
+
+
+
---
+++ menu_productos.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a La Rivera Cafe</title>
+    <link rel="stylesheet" href="styles_bienvenida.css">
+    <link rel="stylesheet" href="styles_menu_productos.css">
+    <link rel="stylesheet" href="styles1.css">
+
+    <title>Document</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a La Rivera Cafe</title>
+    <link rel="stylesheet" href="styles_bienvenida.css">
+</head>
+<body>
+           
+
+            <div class="header">
+                <h1>Bienvenido a "La Rivera Cafe" ¡Una experiencia con aroma a cafe!.</h1>
+            </div>
+
+                    
+        <div class="sidebar">
+            <a href="menu_productos.php" class="modulo" id="productos">
+                <h2>Productos</h2>
+                <!-- Contenido del menú -->
+            </a>                                             
+         </div>
+
+         <body>
+
+         <div class="container">
+    <h1>Productos de La Rivera Cafe</h1>
+    <div class="grid">
+        <div class="producto">
+            <img src="img/mojarra.png" alt="Producto 1">
+            <h2>mojarra frita</h2>
+            <p>Mojarra de 500 gramos, porción de arroz, porción de papitas fritas a la francesa, ensalada y limonada natural.</p>
+            <p>Precio: $36.000.00</p>
+        </div>
+        <div class="producto">
+            <img src="img/trucha gratinada.jpg" alt="Producto 2">
+            <h2>trucha gratinada</h2>
+            <p>Trucha de 500 gramos, porción de arroz, porción de papitas fritas a la francesa, ensalada dulce y limonada natural.</p>
+            <p>Precio: $36.000.00</p>
+        </div>
+    </div> <!-- Cierre de grid -->
+    <div class="grid">
+        <div class="producto">
+            <img src="img/sancocho_gallina.jpg" alt="Producto 3">
+            <h2>sancocho de gallina</h2>
+            <p>Sancocho campesino, con presa de gallina a la brasa.</p>
+            <p>Precio: $30.000.00</p>
+        </div>
+        <div class="producto">
+            <img src="img/imagen 3.jpg" alt="Producto 4">
+            <h2>costillitas al horno</h2>
+            <p>Mojarra de 500 gramos, porción de arroz, porción de papitas fritas a la francesa, ensalada y limonada natural.</p>
+            <p>Precio: $36.000.00</p>
+        </div>
+    </div> <!-- Cierre de grid -->
+</div> <!-- Cierre de container -->
+
+        
+        <!-- Agrega más productos según sea necesario -->
+    </div>
+</div>
+
+</body>
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; 2024 "Restaurante la rivera cafe". Todos los derechos reservados.</p>
+        </div>
+    </footer>
+
+
+    <script src="PHP/menu_productos.js"></script>
+
+	
+</body>
+</html>
